64a6dc6e5f3fb9b1cd3827cd5bb838910679cbf3
novapratiwi <nova.iramma007@gmail.com>
2020-12-25 21:46:46 +0800

    nyawa katak

--- MAIN PART/main.cpp
+++ MAIN PART/main.cpp
@@ -21,6 +21,8 @@ float gerakVertikal4=0;
 
 float kecepatan=0.2;
 
+int nyawa=3;
+
 void init(){
     glClearColor(120,120,120,120);
 }
@@ -663,6 +665,77 @@ void pohon(){
     glPopMatrix();
 }
 
+void hati() {
+    glPushMatrix();
+    glTranslated(0,22,0);
+    glScaled(0.2,0.5,0);
+    glBegin(GL_POLYGON);
+    glColor3f(1.0,0.0,0.0);
+    glVertex2f(15,15); //A
+    glVertex2f(15.5,15.5); //B
+    glVertex2f(16,15.8); //C
+    glVertex2f(16.5,16); //D
+    glVertex2f(17,16); //E
+    glVertex2f(17.4,15.8); //F
+    glVertex2f(17.6,15.4); //G
+    glVertex2f(17.6,14.8); //H
+    glVertex2f(17.4,14.4); //I
+    glVertex2f(17,14); //J
+    glVertex2f(16.6,13.8); //K
+    glVertex2f(16.2,13.6); //L
+    glVertex2f(15.8,13.4); //M
+    glVertex2f(15.4,13.2); //N
+    glVertex2f(15,13); //O
+    glVertex2f(14.6,13.2); //P
+    glVertex2f(14.2,13.4); //Q
+    glVertex2f(13.8,13.6); //R
+    glVertex2f(13.4,13.8); //S
+    glVertex2f(13,14); //T
+    glVertex2f(12.6,14.4); //U
+    glVertex2f(12.4,14.8); //V
+    glVertex2f(12.4,15.4); //W
+    glVertex2f(12.6,15.8); //Z
+    glVertex2f(13,16); //A1
+    glVertex2f(13.5,16); //B1
+    glVertex2f(14,15.8); //C1
+    glVertex2f(14.5,15.5); //D1
+    glEnd();
+
+    glLineWidth(3);
+    glBegin(GL_LINE_LOOP);
+    glColor3f(0.0,0.0,0.0);
+    glVertex2f(15,15); //A
+    glVertex2f(15.5,15.5); //B
+    glVertex2f(16,15.8); //C
+    glVertex2f(16.5,16); //D
+    glVertex2f(17,16); //E
+    glVertex2f(17.4,15.8); //F
+    glVertex2f(17.6,15.4); //G
+    glVertex2f(17.6,14.8); //H
+    glVertex2f(17.4,14.4); //I
+    glVertex2f(17,14); //J
+    glVertex2f(16.6,13.8); //K
+    glVertex2f(16.2,13.6); //L
+    glVertex2f(15.8,13.4); //M
+    glVertex2f(15.4,13.2); //N
+    glVertex2f(15,13); //O
+    glVertex2f(14.6,13.2); //P
+    glVertex2f(14.2,13.4); //Q
+    glVertex2f(13.8,13.6); //R
+    glVertex2f(13.4,13.8); //S
+    glVertex2f(13,14); //T
+    glVertex2f(12.6,14.4); //U
+    glVertex2f(12.4,14.8); //V
+    glVertex2f(12.4,15.4); //W
+    glVertex2f(12.6,15.8); //Z
+    glVertex2f(13,16); //A1
+    glVertex2f(13.5,16); //B1
+    glVertex2f(14,15.8); //C1
+    glVertex2f(14.5,15.5); //D1
+    glEnd();
+    glPopMatrix();
+}
+
 void gambarKatak(){
     //BADAN
     glBegin(GL_POLYGON);
@@ -1225,6 +1298,7 @@ void timerMobil1(int){
         gerakVertikal1=40;
         colliderMobil1[0]=30;
         colliderMobil1[2]=25;
+        nyawa-=1;
     }
 
     glutPostRedisplay();
@@ -1617,6 +1691,7 @@ void timerMobil2(int){
            gerakVertikal2 = 0;
            colliderMobil2[0]=-30;
            colliderMobil2[2]=-25;
+           nyawa-=1;
     }
 
     glutPostRedisplay();
@@ -2008,6 +2083,7 @@ void timerMobil3(int){
         gerakVertikal3=40;
         colliderMobil3[0]=30;
         colliderMobil3[2]=25;
+        nyawa-=1;
     }
 
     glutPostRedisplay();
@@ -2400,6 +2476,7 @@ void timerMobil4(int){
         gerakVertikal4=0;
         colliderMobil4[0]=-30;
         colliderMobil4[2]=-25;
+        nyawa-=1;
     }
 
     glutPostRedisplay();
@@ -2454,6 +2531,16 @@ void katakInput(int id) {
     glutTimerFunc(40, katakInput, 0);
 }
 
+void displayNyawa() {
+    float x = 0;
+    for (int i = 0; i < nyawa; ++i) {
+        glPushMatrix();
+        glTranslated(x,0,0);
+        hati();
+        glPopMatrix();
+        x -= 1.2;
+    }
+}
 
 void displayMe(void){
     glClear(GL_COLOR_BUFFER_BIT);
@@ -2464,6 +2551,7 @@ void displayMe(void){
     mobil2();
     mobil3();
     mobil4();
+    displayNyawa();
     glFlush();
     glutSwapBuffers();
 }
@@ -2481,7 +2569,7 @@ int main(int argc, char** argv){ // untuk jendelanya
 	glutTimerFunc(0,timerMobil4,0);
 	glutTimerFunc(0, katakInput, 0);
 	init();
-	gluOrtho2D(0,30,0,30); // inisialisasi berapa kotak yg mau dipakai
+	gluOrtho2D(0,30,0,31); // inisialisasi berapa kotak yg mau dipakai
 	glutMainLoop();//untuk menjalan kan program, semua fungsi dieksekusi
 	return 0;
 }
